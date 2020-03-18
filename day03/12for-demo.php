9f0ce1d4c589b5d41a81580e25bf671d1ee0482b
Administrator <362641643@qq.com>
2020-03-18 18:02:46 +0800

    增加 循环语句

---
+++ day03/12for-demo.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// 输出1-100
+for($s=1;$s<=100; ++$s){
+    echo '<br>值为',$s;
+}
+?>
+</body>
+</html>
